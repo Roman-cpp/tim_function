4838a6d0b774ed36ff321473992a12d96c9c411d
Roman-cpp <85461255+Roman-cpp@users.noreply.github.com>
2022-04-18 12:07:14 +0300

    added class

--- main.php
+++ main.php
@@ -1,26 +1,27 @@
 <?php
-function TimeUnix($mark_Unix): string
+Class date
 {
-    $month = "";
-    $i = date("m", $mark_Unix);
-    $month = match ((int)$i) {
-        1 => "Январь",
-        2 => "Февраль",
-        3 => "Март",
-        4 => "Апрель",
-        5 => "Май",
-        6 => "Июнь",
-        7 => "Июль",
-        8 => "Август",
-        9 => "Сентябрь",
-        10 => "Октябрь",
-        11 => "Ноябрь",
-        12 => "Декабрь",
-    };
-
-
-    return date("d", $mark_Unix) . " " . $month . " " . date("Y", $mark_Unix);
+    function TimeUnix($mark_Unix): string
+    {
+        $month = "";
+        $i = date("m", $mark_Unix);
+        $month = match ((int)$i) {
+            1 => "Январь",
+            2 => "Февраль",
+            3 => "Март",
+            4 => "Апрель",
+            5 => "Май",
+            6 => "Июнь",
+            7 => "Июль",
+            8 => "Август",
+            9 => "Сентябрь",
+            10 => "Октябрь",
+            11 => "Ноябрь",
+            12 => "Декабрь",
+        };
+        return date("d", $mark_Unix) . " " . $month . " " . date("Y", $mark_Unix);
+    }
 }
+$date_= new date();
 
-$date_unix= $_POST["mark_Unix"];
-echo TimeUnix($date_unix);
+echo $date_->TimeUnix($_POST["mark_Unix"]);
